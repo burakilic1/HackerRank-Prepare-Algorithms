73d3bf944b8d8a6da3b32e2a892b842961c3637c
burakilic1 <119857689+burakilic1@users.noreply.github.com>
2022-12-25 23:08:41 +0300

    New Examples

---
+++ Plus Minus/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int n = arr.Count;
+        decimal sump = 0;
+        decimal sumn = 0;
+        decimal sumz = 0;
+        foreach (var x in arr)
+        {
+            if(x>0)
+                sump++;
+            else if(x<0)
+                sumn++;
+            else
+                sumz++;
+        }
+
+        Console.WriteLine($"{sump / n:0.000000}");
+        Console.WriteLine($"{sumn / n:0.000000}");
+        Console.WriteLine($"{sumz / n:0.000000}");
+        Console.ReadLine();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
---
+++ Time Conversion/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Globalization;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        string AmOrPm = s.Substring(8);
+        string hour = s.Substring(0, 2);
+        string between = s.Substring(2, 6);
+        if (AmOrPm == "AM" && hour == "12")
+        {
+            hour = "00";
+        }
+        else if (AmOrPm == "PM")
+        {
+            int numeric = int.Parse(hour);
+            if (numeric != 12)
+            { hour = Convert.ToString(12 + numeric); }
+
+        }
+       Console.Write(hour + between);
+
+        return AmOrPm;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+}

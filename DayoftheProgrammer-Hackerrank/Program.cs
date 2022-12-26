7fdf829071c95ec4176d00babb15e6f2467090bc
burakilic1 <119857689+burakilic1@users.noreply.github.com>
2022-12-26 20:40:49 +0300

    Add  Examples

---
+++ DayoftheProgrammer-Hackerrank/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static string dayOfProgrammer(int year)
+    {
+        string ddmm1 = "12.09.";
+        string ddmm2 = "13.09.";
+        if (year == 1918)
+            return "26.09.1918";
+        if (year<1918)
+        {
+            if (year % 4 == 0)
+            {
+                return ddmm1 + year.ToString();
+            }
+                return ddmm2 + year.ToString();
+        }
+        else
+        {
+            if ((year%4==0 && year%100!=0) || (year%100==0 && year%400==0))
+            {
+                return ddmm1 + year.ToString();
+            }
+        }
+        return ddmm2 + year.ToString();
+
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+     
+        int year = Convert.ToInt32(Console.ReadLine().Trim());
+
+        string result = Result.dayOfProgrammer(year);
+
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+
+    }
+}
+
---
+++ DivisibleSumPairs-HackerRanks/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+using System.Security.Cryptography;
+
+class Result
+{
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int sum = 0;
+        int c = 0;
+        for (int i = 0; i < ar.Count; i++)
+        {
+            for(int j=0; j< ar.Count;j++)
+            {
+                if(i!=j)
+                {
+                    c = ar[i] + ar[j];
+                    if (c>1 && c%k == 0 )
+                    {
+                        sum++;
+                    }
+                    
+                }
+                else
+                {
+
+                }
+            }
+        }
+        sum = sum / 2;
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+
+    }
+}
+ 

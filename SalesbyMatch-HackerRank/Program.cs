944355675b1d584b8f56604273412e8e379b05e2
burakilic1 <119857689+burakilic1@users.noreply.github.com>
2022-12-27 14:27:43 +0300

    Add Examples

---
+++ SalesbyMatch-HackerRank/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+       int sockCount = 0;
+       var set = new HashSet<int>();
+
+        foreach (var x in ar)
+        {
+            if (!set.Contains(x)) 
+            {
+                set.Add(x);
+            }
+            else
+            {
+                sockCount++;    
+                set.Remove(x);
+            }
+        }
+        return sockCount;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        Console.WriteLine(result);
+
+        Console.ReadLine();
+    }
+}
